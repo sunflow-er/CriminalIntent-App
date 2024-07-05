0ed412d46ac18fa846eaa8479e61b3de06d22106
sunflow-er <antjd03170@gmail.com>
2024-07-05 15:30:20 +0900

    Adapter, ViewHolder 역할 분리

--- app/src/main/java/org/javaapp/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/org/javaapp/criminalintent/CrimeListFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -18,8 +19,10 @@ private const val TAG = "CrimeListFragment"
 class CrimeListFragment : Fragment() {
 
     private lateinit var crimeRecyclerView: RecyclerView
+    private var adapter: CrimeAdapter? = null
 
-    private val crimeListViewModel : CrimeListViewModel by lazy {
+    // 뷰모델 연결
+    private val crimeListViewModel: CrimeListViewModel by lazy {
         ViewModelProvider(this).get(CrimeListViewModel::class.java)
     }
 
@@ -38,16 +41,41 @@ class CrimeListFragment : Fragment() {
         crimeRecyclerView = view.findViewById(R.id.crime_recycler_view) as RecyclerView
         crimeRecyclerView.layoutManager = LinearLayoutManager(context) // LayoutManager 바로 설정
 
+        updateUI()
+
         return view
     }
 
-    private inner class CrimeHolder(view : View) : RecyclerView.ViewHolder(view) {
-        // view(item view)의 TextView 참조
-        val titleTextView : TextView = itemView.findViewById(R.id.crime_title)
-        val dateTextView : TextView = itemView.findViewById(R.id.crime_date)
+    private fun updateUI() {
+        val crimes = crimeListViewModel.crimes
+        adapter = CrimeAdapter(crimes)
+        crimeRecyclerView.adapter = adapter
     }
 
-    private inner class CrimeAdapter(var crimes : List<Crime>) : RecyclerView.Adapter<CrimeHolder>() {
+    private inner class CrimeHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener {
+        private lateinit var crime : Crime
+
+        // view(item view)의 TextView 참조
+        private val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
+        private val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        fun bind(crime : Crime) {
+            this.crime = crime
+            titleTextView.text = this.crime.title
+            dateTextView.text = this.crime.date.toString()
+        }
+
+        override fun onClick(v: View?) {
+            Toast.makeText(context, "${crime.title} pressed!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private inner class CrimeAdapter(var crimes: List<Crime>) :
+        RecyclerView.Adapter<CrimeHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
             val view = layoutInflater.inflate(R.layout.list_item_crime, parent, false)
 
@@ -60,16 +88,13 @@ class CrimeListFragment : Fragment() {
 
         override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
             val crime = crimes[position]
-            holder.apply {
-                titleTextView.text = crime.title
-                dateTextView.text = crime.date.toString()
-            }
+            holder.bind(crime)
         }
 
     }
 
     companion object {
-        fun newInstance() : CrimeListFragment {
+        fun newInstance(): CrimeListFragment {
             return CrimeListFragment()
         }
     }
