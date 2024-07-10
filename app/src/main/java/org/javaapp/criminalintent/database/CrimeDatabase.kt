e8df82b613100f666df027f2dd1212cc22bca3ac
sunflow-er <antjd03170@gmail.com>
2024-07-10 22:39:29 +0900

    타입변환기 클래스를 데이터베이스 클래스에 추가

--- app/src/main/java/org/javaapp/criminalintent/database/CrimeDatabase.kt
+++ app/src/main/java/org/javaapp/criminalintent/database/CrimeDatabase.kt
@@ -2,8 +2,11 @@ package org.javaapp.criminalintent.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
 import org.javaapp.criminalintent.Crime
 
 @Database (entities = [ Crime::class ], version = 1)
+@TypeConverters(CrimeTypeConverters::class)
 abstract class CrimeDatabase : RoomDatabase() {
 }
