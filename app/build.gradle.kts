72f70fa73cd825d6d5124738da28ac915cdf58b1
sunflow-er <antjd03170@gmail.com>
2024-07-10 22:06:26 +0900

    Room 데이터베이스 entity 정의

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,9 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
+    val room_version = "2.6.1"
+
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+
 }
--- app/src/main/java/org/javaapp/criminalintent/Crime.kt
+++ app/src/main/java/org/javaapp/criminalintent/Crime.kt
@@ -1,9 +1,12 @@
 package org.javaapp.criminalintent
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.Date
 import java.util.UUID
 
-data class Crime(val id: UUID = UUID.randomUUID(), // Universally Unique Identifier, 임의의 고유한 ID값을 생성
+@Entity
+data class Crime(@PrimaryKey val id: UUID = UUID.randomUUID(), // Universally Unique Identifier, 임의의 고유한 ID값을 생성
                  var title: String = "",
                  var date: Date = Date(), // 현재 일자로 초기화
                  var isSolved: Boolean = false)
