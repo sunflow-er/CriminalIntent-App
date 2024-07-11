00f87f9e2f37a67555ba451f548ce89a281e9913
sunflow-er <antjd03170@gmail.com>
2024-07-11 19:13:26 +0900

    프래그먼트 간 이동을 위한 콜백 인터페이스 구현

--- app/src/main/java/org/javaapp/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/org/javaapp/criminalintent/CrimeListFragment.kt
@@ -1,5 +1,6 @@
 package org.javaapp.criminalintent
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -15,10 +16,19 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Recycler
 import org.w3c.dom.Text
+import java.util.UUID
 
 private const val TAG = "CrimeListFragment"
 
 class CrimeListFragment : Fragment() {
+    /**
+     * 호스팅 액티비티에서 구현할 인터페이스
+     */
+    interface Callbacks {
+        fun onCrimeSelected(crimeId: UUID)
+    }
+
+    private var callbacks: Callbacks? = null // Callbacks 인터페이스를 구현하는 객체를 참조하기 위한 속성
 
     private lateinit var crimeRecyclerView: RecyclerView
     private var adapter: CrimeAdapter? = CrimeAdapter(emptyList())
@@ -26,6 +36,18 @@ class CrimeListFragment : Fragment() {
     // 뷰모델 연결
     private val crimeListViewModel: CrimeListViewModel by lazy {
         ViewModelProvider(this).get(CrimeListViewModel::class.java)
+    }
+
+    // callbacks 속성을 설정
+    override fun onAttach(context: Context) { // 프래그먼트가 호스팅 액티비티에 연결될 때 호출
+        super.onAttach(context)
+        
+        /*
+        호스팅 액티비티 인스턴스인 Context 객체를 Callbacks 인터페이스 타입으로 캐스팅
+        이렇게 함으로써 프래그먼트는 Callbacks 인터페이스를 구현하는 액티비티의 인스턴스에 접근할 수 있게 됨
+        이 인스턴스를 통해 프래그먼트는 액티비티에 정의된 메서드를 호출할 수 있음
+         */
+        callbacks = context as Callbacks
     }
 
     override fun onCreateView(
@@ -45,9 +67,10 @@ class CrimeListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // crimeListViewModel의 crimeListLiveData를 관찰하고 변경될 때마다 UI 업데이트
         crimeListViewModel.crimeListLiveData.observe(
-            viewLifecycleOwner,
-            Observer { crimes ->
+            viewLifecycleOwner, // 프래그먼트의 뷰와 관련된 생명주기 이벤트를 관찰하는 데 사용, 프래그먼트의 뷰가 화면에 있을 때만 업데이트가 수행
+            Observer { crimes -> // Observer 객체를 익명클래스로 생성, 관찰되는 데이터가 변경될 때 호출될 콜백 함수 정의
                 crimes?.let {
                     Log.i(TAG, "Got crimes ${crimes.size}")
                     updateUI(crimes)
@@ -56,11 +79,18 @@ class CrimeListFragment : Fragment() {
         )
     }
 
+    // callbacks 속성을 설정 해제
+    override fun onDetach() { // 프래그먼트가 액티비티에서 분리될 때 호출
+        super.onDetach()
+        callbacks = null
+    }
+
     private fun updateUI(crimes: List<Crime>) {
         adapter = CrimeAdapter(crimes)
         crimeRecyclerView.adapter = adapter
     }
 
+    // ViewHolder
     private inner class CrimeHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener {
         private lateinit var crime : Crime
 
@@ -85,10 +115,11 @@ class CrimeListFragment : Fragment() {
         }
 
         override fun onClick(v: View?) {
-            Toast.makeText(context, "${crime.title} pressed!", Toast.LENGTH_SHORT).show()
+            callbacks?.onCrimeSelected(crime.id)
         }
     }
 
+    // RecyclerView Adapter
     private inner class CrimeAdapter(var crimes: List<Crime>) :
         RecyclerView.Adapter<CrimeHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
--- app/src/main/java/org/javaapp/criminalintent/MainActivity.kt
+++ app/src/main/java/org/javaapp/criminalintent/MainActivity.kt
@@ -6,8 +6,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.UUID
 
-class MainActivity : AppCompatActivity() {
+private const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity(), CrimeListFragment.Callbacks{
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,4 +23,10 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.beginTransaction().add(R.id.fragment_container, fragment).commit() // 프래그먼트 트랜잭션을 생성하고 커밋
         }
     }
+
+    override fun onCrimeSelected(crimeId: UUID) {
+        Log.d(TAG, "MainActivity.onCrimeSelected: $crimeId")
+    }
+
+
 }
