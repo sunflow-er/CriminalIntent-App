3fe7a16c18cce3565fdccd8376118c99093162d9
sunflow-er <antjd03170@gmail.com>
2024-07-14 12:07:36 +0900

    DatePickerFragment(DialogFragment)가 전달받은 데이터를 사용

--- app/src/main/java/org/javaapp/criminalintent/DatePickerFragment.kt
+++ app/src/main/java/org/javaapp/criminalintent/DatePickerFragment.kt
@@ -13,7 +13,16 @@ class DatePickerFragment : DialogFragment() {
 
     // DialogFragment를 보여주려고 호스팅 액티비티의 FragmentManager가 이 함수를 호출
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val date = arguments?.getSerializable(ARG_DATE) as Date // 프래그먼트 인자로부터 Date 객체 정보 가져오기
+
+        /*
+        Date 객체는 타임스탬프 형식이므로 월, 일, 년 값을 바로 추출할 수 없다.
+        따라서 Date 객체를 사용해서 Calendar 객체를 생성 후, 이 Calendar 객체로부터 필요한 값을 추출한다.
+         */
         val calendar = Calendar.getInstance()
+        calendar.time = date // calendar의 현재 시간을 주어진 date 객체로 맞춘다.
+
+
         val initialYear = calendar.get(Calendar.YEAR)
         val initialMonth = calendar.get(Calendar.MONTH)
         val initialDay = calendar.get(Calendar.DAY_OF_MONTH)
